26a309ec4561fa3fcd1e82d42ca43c0bd385918a
Oleg Alexandrov <oleg.alexandrov@gmail.com>
2021-06-16 11:26:40 -0700

    Update to build msmw and msmw2

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,14 +5,60 @@ set -e
 export CFLAGS="-I$PREFIX/include -O3 -DNDEBUG -ffast-math -march=native"
 export LDFLAGS="-L$PREFIX/lib"
 
-# Fix for missing liblzma.
+# Fix for missing liblzma
 perl -pi -e "s#(/[^\s]*?lib)/lib([^\s]+).la#-L\$1 -l\$2#g" ${PREFIX}/lib/*.la
 
-make -j${CPU_COUNT}
+baseDir=$(pwd)
 
-# Copy this program to the plugins directory
+if [ "$(uname)" = "Darwin" ]; then
+    EXT='.dylib'
+else
+    EXT='.so'
+fi
+
+# Build the desired programs
+
+cd 3rdparty/mgm
+perl -pi -e "s#CFLAGS=#CFLAGS=$CFLAGS #g" Makefile
+perl -pi -e "s#LDFLAGS=#LDFLAGS=$LDFLAGS #g" Makefile 
+make -j${CPU_COUNT}
+cd $baseDir
+
+cd 3rdparty/msmw
+mkdir -p build
+cd build
+cmake .. -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CFLAGS" \
+    -DJPEG_LIBRARY="${PREFIX}/lib/libjpeg${EXT}"
+make -j${CPU_COUNT}
+cd $baseDir
+
+cd 3rdparty/msmw2
+mkdir -p build
+cd build
+cmake ..                                                   \
+    -DCMAKE_C_FLAGS="$CFLAGS" -DCMAKE_CXX_FLAGS="$CFLAGS"  \
+    -DPNG_LIBRARY_RELEASE="${PREFIX}/lib/libpng${EXT}"     \
+    -DTIF_LIBRARY_RELEASE="${PREFIX}/lib/libtif${EXT}"     \
+    -DZLIB_LIBRARY_RELEASE="${PREFIX}/lib/libz${EXT}"      \
+    -DJPEG_LIBRARY="${PREFIX}/lib/libjpeg${EXT}"
+make -j${CPU_COUNT}
+cd $baseDir
+
+# Install the desired programs
+
+BIN_DIR=${PREFIX}/plugins/stereo/msmw2/bin
+mkdir -p ${BIN_DIR}
+/bin/cp -fv \
+    3rdparty/msmw2/build/libstereo_newversion/iip_stereo_correlation_multi_win2_newversion \
+    ${BIN_DIR}/msmw2
+
 BIN_DIR=${PREFIX}/plugins/stereo/mgm/bin
 mkdir -p ${BIN_DIR}
-/bin/cp -fv mgm ${BIN_DIR}
+/bin/cp -fv 3rdparty/mgm/mgm ${BIN_DIR}
 
+BIN_DIR=${PREFIX}/plugins/stereo/msmw/bin
+mkdir -p ${BIN_DIR}
+/bin/cp -fv \
+    3rdparty/msmw/build/libstereo/iip_stereo_correlation_multi_win2 \
+    ${BIN_DIR}/msmw
 
